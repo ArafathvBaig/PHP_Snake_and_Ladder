4db755bc67f8beec11e2b75d036a07be9866835d
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-23 02:11:16 +0530

    Added: Welcome message to the master

---
+++ src/Snake_and_Ladder.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Class for Snake and Ladder Computation Problem
+ */
+class Snake_and_Ladder
+{
+    function welcomeMessage()
+    {
+        echo "Welcome to Snake and Ladder Computation Problem\n";
+    }
+}
+
+$snakeAndLadder = new Snake_and_Ladder();
+$snakeAndLadder->welcomeMessage();
